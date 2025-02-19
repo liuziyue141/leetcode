cd987572a7ad7d66e41071874840904452e0c473
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-02-19 11:56:03 -0800

    Time: 44 ms (5.73%), Space: 51.1 MB (7.59%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -6,35 +6,39 @@ class Solution {
             this.x = x;
             this.y = y;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Pair)) return false;
+            Pair pair = (Pair) o;
+            return x == pair.x && y == pair.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        public String toString(){
+            return this.x + " " + this.y;
+        }
     }
     public int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<Pair> pq = new PriorityQueue<>((Pair a, Pair b) -> matrix[a.x][a.y] - matrix[b.x][b.y]);
+        HashSet<Pair> set = new HashSet<>();
         pq.add(new Pair(0,0));
+        set.add(new Pair(0,0));
         Pair smallest = null;
         for(int i = 0; i < k; i++){
             smallest = pq.poll();
-            boolean addFChild = true;
-            boolean addSChild = true;
-            if(smallest.x+1 >= matrix.length){
-                addFChild = false;
-            }
-            if(smallest.y+1 >= matrix.length){
-                addSChild = false;
-            }
-            for(Pair pair : pq){
-                if(smallest.x+1 >= pair.x && smallest.y >= pair.y){
-                    addFChild = false;
-                }
-                if(smallest.x >= pair.x && smallest.y+1 >= pair.y){
-                    addSChild = false;
-                }
-            }
-
-            if(addFChild){
+            if(smallest.x+1 < matrix.length && !set.contains(new Pair(smallest.x+1, smallest.y))){
                 pq.add(new Pair(smallest.x+1, smallest.y));
+                set.add(new Pair(smallest.x+1, smallest.y));
             }
-            if(addSChild){
+            if(smallest.y+1 < matrix.length && !set.contains(new Pair(smallest.x, smallest.y+1))){
                 pq.add(new Pair(smallest.x, smallest.y+1));
+                set.add(new Pair(smallest.x, smallest.y+1));
             }
         }
 
