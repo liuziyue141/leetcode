7ea4a2ed80f711744e24c887272146d72a365f3f
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-02-19 12:47:29 -0800

    Time: 4 ms (67.29%), Space: 49.9 MB (31.55%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,47 +1,34 @@
 class Solution {
-    class Pair{
-        int x;
-        int y;
-        public Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Pair)) return false;
-            Pair pair = (Pair) o;
-            return x == pair.x && y == pair.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-
-        public String toString(){
-            return this.x + " " + this.y;
-        }
-    }
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair a, Pair b) -> matrix[a.x][a.y] - matrix[b.x][b.y]);
-        HashSet<Pair> set = new HashSet<>();
-        pq.add(new Pair(0,0));
-        set.add(new Pair(0,0));
-        Pair smallest = null;
-        for(int i = 0; i < k; i++){
-            smallest = pq.poll();
-            if(smallest.x+1 < matrix.length && !set.contains(new Pair(smallest.x+1, smallest.y))){
-                pq.add(new Pair(smallest.x+1, smallest.y));
-                set.add(new Pair(smallest.x+1, smallest.y));
+        //range binary sort solution:
+
+        int hi = matrix[matrix.length-1][matrix.length-1];
+        int lo = matrix[0][0];
+
+        while(lo<=hi){
+            int mid = (hi+lo)/2;
+
+            int count = 0;
+            int maxNum = -10000;
+
+            // invariant established here
+            for(int i = 0; i < matrix.length; i++){
+                int j = 0;
+                while(j < matrix.length && matrix[i][j]<=mid){
+                    maxNum = Math.max(maxNum, matrix[i][j]);
+                    j++;
+                }
+                count += j;
             }
-            if(smallest.y+1 < matrix.length && !set.contains(new Pair(smallest.x, smallest.y+1))){
-                pq.add(new Pair(smallest.x, smallest.y+1));
-                set.add(new Pair(smallest.x, smallest.y+1));
+
+            if(count == k){
+                return maxNum;
+            }else if(count < k){
+                lo = mid + 1;
+            }else{
+                hi = mid -1;
             }
         }
-
-        return matrix[smallest.x][smallest.y];
+        return lo;
     }
 }
