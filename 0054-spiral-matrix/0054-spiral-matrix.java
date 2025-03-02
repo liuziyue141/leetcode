a422053c70827d5b688583e02cae135ab54c6d06
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-03-02 10:35:19 -0800

    Time: 0 ms (100%), Space: 42 MB (8.59%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         int[][] delta = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int m = matrix.length;
-        int n = matrix[0].length;
-        boolean[][] seen = new boolean[m][n];
+        int m = matrix.length; int n = matrix[0].length;
         List<Integer> res = new ArrayList<>();
-        boolean hasChange = true;
         
-        int r = 0;
-        int c = 0;
-        boolean modified = true;
-        int cnt = 0;
-        int dirCnt = 0;
+        int r = 0; int c = 0; int dirCnt = 0;
+        int up = 0; int down = m; int left = 0; int right = n;
 
-        while (modified){
-            modified = false;
-            while(r >=0 && c >=0 && r < m && c < n && !seen[r][c]){
-                modified = true;
-                seen[r][c] = true;
+        while (up < down && left < right){
+            while(r >=up && c >=left && r < down && c < right){
                 res.add(matrix[r][c]);
-                cnt++;
                 r += delta[dirCnt][0];
                 c += delta[dirCnt][1];
+            }
+            if(dirCnt == 0){
+                up++;
+            }else if(dirCnt == 1){
+                right--;
+            }else if(dirCnt == 2){
+                down--;
+            }else{
+                left++;
             }
             r -= delta[dirCnt][0];
             c -= delta[dirCnt][1];
             dirCnt = (dirCnt + 1)%4;
             r += delta[dirCnt][0];
             c += delta[dirCnt][1];
+            
         }
         return res;
     }
