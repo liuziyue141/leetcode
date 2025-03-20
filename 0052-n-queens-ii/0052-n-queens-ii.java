45f92f082140ce84073b592f079cf71493aa04a9
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-03-20 14:42:42 -0700

    Time: 3 ms (28.61%), Space: 41.5 MB (16.27%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -7,37 +7,25 @@ class Solution {
         posDiag = new HashSet<>();
         negDiag = new HashSet<>();
         cols = new HashSet<>();
-        totalNQueens(0, 0, n);
+        totalNQueens(0, n);
         return res;
     }
 
-    public void totalNQueens(int row, int col, int n){
-        if(col == n){
-            return;
-        }
+    public void totalNQueens(int row, int n){
         if(row == n){
             res = res + 1;
             return;
         }
-        if(!cols.contains(col) && !posDiag.contains(row + col) && !negDiag.contains(row - col)){
-            cols.add(col);
-            posDiag.add(row + col);
-            negDiag.add(row - col);
-            totalNQueens(row + 1, 0, n);
-            cols.remove(col);
-            posDiag.remove(row + col);
-            negDiag.remove(row - col);
-        }
-        totalNQueens(row, col + 1, n);
-    }
-
-    public boolean validPos(List<int[]> positions, int row, int col){
-        for(int[] position : positions){
-            if(Math.abs(position[0] - row) == Math.abs(position[1] - col) || position[0] == row || position[1] == col){
-                return false;
+        for(int col = 0; col < n; col++){
+            if(!cols.contains(col) && !posDiag.contains(row + col) && !negDiag.contains(row - col)){
+                cols.add(col);
+                posDiag.add(row + col);
+                negDiag.add(row - col);
+                totalNQueens(row + 1, n);
+                cols.remove(col);
+                posDiag.remove(row + col);
+                negDiag.remove(row - col);
             }
         }
-
-        return true;
     }
 }
