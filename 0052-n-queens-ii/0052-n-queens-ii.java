cc30f14ddb321a387d32a313e12cfae826776fc9
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-03-20 14:40:30 -0700

    Time: 4 ms (22.23%), Space: 41.8 MB (12.05%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,11 +1,17 @@
 class Solution {
     int res = 0;
+    Set<Integer> cols;
+    Set<Integer> posDiag;
+    Set<Integer> negDiag;
     public int totalNQueens(int n) {
-        totalNQueens(0, 0, n, new ArrayList<>());
+        posDiag = new HashSet<>();
+        negDiag = new HashSet<>();
+        cols = new HashSet<>();
+        totalNQueens(0, 0, n);
         return res;
     }
 
-    public void totalNQueens(int row, int col, int n, List<int[]> positions){
+    public void totalNQueens(int row, int col, int n){
         if(col == n){
             return;
         }
@@ -13,12 +19,16 @@ class Solution {
             res = res + 1;
             return;
         }
-        if(validPos(positions, row, col)){
-            positions.add(new int[]{row, col});
-            totalNQueens(row + 1, 0, n, positions);
-            positions.remove(positions.size()-1);
+        if(!cols.contains(col) && !posDiag.contains(row + col) && !negDiag.contains(row - col)){
+            cols.add(col);
+            posDiag.add(row + col);
+            negDiag.add(row - col);
+            totalNQueens(row + 1, 0, n);
+            cols.remove(col);
+            posDiag.remove(row + col);
+            negDiag.remove(row - col);
         }
-        totalNQueens(row, col + 1, n, positions);
+        totalNQueens(row, col + 1, n);
     }
 
     public boolean validPos(List<int[]> positions, int row, int col){
