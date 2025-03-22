98397f94bf3156c89e46e156199f1f29a8756253
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-03-22 13:46:25 -0700

    Time: 2 ms (96.06%), Space: 43.7 MB (60.44%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,72 +1,29 @@
 class Solution {
     int i = 0;
     public int calculate(String s) {
-        Stack<Integer> numStack = new Stack<>();
-        Stack<Character> charStack = new Stack<>();
-        boolean numStart = false;
-        int numStartIdx = 0;
-        int numEndIdx = 0;
+        int cur = 0;
+        int sign = 1;
+        int res = 0;
         while(i < s.length()){
             char c = s.charAt(i);
-            if(c == ' '){
+            if(Character.isDigit(c)){
+                cur = cur * 10 + c - '0';
+            }else if(c == '-'){
+                res = res + sign * cur;
+                cur = 0;
+                sign = -1;
+            }else if( c == '+'){
+                res = res + sign * cur;
+                cur = 0;
+                sign = 1;
+            }else if(c == '('){
                 i++;
-                continue;
-            }
-
-            // handle multiple digits
-            else if(isDigit(c)){
-                if(!numStart){
-                    numStartIdx = i;
-                    numStart = true;
-                }
-
-                if(i==s.length()-1 || !isDigit(s.charAt(i+1))){
-                    numEndIdx = i + 1;
-                    numStart = false;
-                    numStack.push(Integer.valueOf(s.substring(numStartIdx, numEndIdx)));
-                    if(!charStack.isEmpty()){
-                        char opt = charStack.pop();
-                        int b = numStack.pop();
-                        int a = numStack.pop();
-
-                        numStack.push(evaluate(a, b, opt));
-                    }
-                }
-            }
-
-            else if(c == '('){
-                i++;
-                numStack.push(calculate(s));
-                if(!charStack.isEmpty()){
-                    char opt = charStack.pop();
-                    int b = numStack.pop();
-                    int a = numStack.pop();
-                    numStack.push(evaluate(a, b, opt));
-                }
+                cur = calculate(s);
             }else if (c == ')'){
-                return numStack.peek();
-            }else{
-                if(c == '-'){
-                    if(numStack.isEmpty()){
-                        numStack.push(0);
-                    }
-                }
-
-                charStack.push(c);
+                return res + sign * cur;
             }
             i++;
         }
-        return numStack.peek();
-    }
-    public boolean isDigit (char c){
-        return c <= '9' && c >= '0';
-    }
-
-    public int evaluate (int a, int b, char opt){
-        if (opt == '-'){
-            return a-b;
-        }else{
-            return a+b;
-        }
+        return res + sign * cur;
     }
 }
