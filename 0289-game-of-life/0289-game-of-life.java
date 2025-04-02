6771ea046ec36137794cf8dcaa3523b8d68b1291
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-04-01 20:12:24 -0700

    Time: 0 ms (100%), Space: 41.7 MB (73.26%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,48 +1,54 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[] prev = new int[board[0].length];
-        int[] cur = new int[board[0].length];
-        // for(int i = 0; i < board[0].length; i++){
-        //     cur[i] = board[0].length;
-        // }
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                int count = countNeighbor(i, j, board);
+                //board[i][j] = board[i][j]<<1;
+                if(count == 3){
+                    if(board[i][j] == 1){
+                        board[i][j] = 4;
+                    }else{
+                        board[i][j] = 3;
+                    }
+                }
+                else if(count == 2){
+                    if(board[i][j] == 1){
+                        board[i][j] = 4;
+                    }else{
+                        board[i][j] = 2;
+                    }
+                }
+                else{
+                    if(board[i][j] == 1){
+                        board[i][j] = 5;
+                    }else{
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
 
         for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                int cnt = prev[j];
-                if(j > 0){
-                    cnt = cnt + prev[j-1] + cur[j-1];
-                }
-                if(j<board[0].length - 1){
-                    cnt = cnt + prev[j+1] + board[i][j+1];
-                }
-                if(i<board.length - 1){
-                    cnt += board[i+1][j];
-                }
-                if(i<board.length - 1 && j<board[0].length - 1){
-                    cnt += board[i+1][j + 1];
-                }
-                if(i<board.length - 1 && j>0){
-                    cnt += board[i+1][j-1];
-                }
-                cur[j] = board[i][j];
-
-                // System.out.println(i + " " + j + " " + cnt);
-
-                if(cnt < 2){
-                    board[i][j] = 0;
-                }
-                if(cnt == 3){
+            for(int j = 0; j < board[i].length; j++){
+                if(board[i][j] == 4 || board[i][j] == 3){
                     board[i][j] = 1;
-                }
-                if(cnt > 3){
+                }else{
                     board[i][j] = 0;
                 }
             }
-
-            for(int j = 0; j < board[0].length; j++){
-                prev[j] = cur[j];
-            }
-            // System.out.println(Arrays.toString(prev));
         }
+    }
+
+    public int countNeighbor(int row, int col, int[][] board){
+        int cnt = 0;
+        for(int i = row - 1; i <= row + 1; i++){
+            for(int j = col - 1; j <= col + 1; j++){
+                if((i==row && j == col) || i < 0 || i >= board.length || j < 0 || j >= board[0].length)
+                    continue;
+                if(board[i][j] == 5 || board[i][j] == 4 || board[i][j] == 1)
+                    cnt++;
+            }
+        }
+        return cnt;
     }
 }
