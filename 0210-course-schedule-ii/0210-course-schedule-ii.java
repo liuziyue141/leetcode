feabe7b3cb1de47b4cbb0698d741c436600f4d6a
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-04-09 18:54:30 -0700

    Time: 6 ms (54.83%), Space: 45.9 MB (62.07%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] inorder = new int[numCourses];
+        List<Set<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
+            graph.add(new HashSet<>());
+        }
+
+        for(int[] prereq : prerequisites){
+            inorder[prereq[0]]++;
+            graph.get(prereq[1]).add(prereq[0]);
+        }
+
+        for(int i = 0; i < numCourses; i++){
+            if(inorder[i] == 0){
+                queue.addLast(i);
+            }
+        }
+        int[] res = new int[numCourses];
+        int idx = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int cur = queue.removeFirst();
+                res[idx] = cur;
+                idx++;
+                Set<Integer> neighbor = graph.get(cur);
+                for(int n : neighbor){
+                    inorder[n]--;
+                    if(inorder[n] == 0){
+                        queue.addLast(n);
+                    }
+                }
+            }
+        }
+        if(idx==numCourses){
+            return res;
+        }else{
+            return new int[0];
+        }
+    }
+}
