55b2a87a1904ae194dbd0e11052f739f4e6cad6b
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-04-16 11:51:17 -0700

    Time: 0 ms (100%), Space: 41.7 MB (41%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,9 +1,23 @@
 class Solution {
+    // public int hIndex(int[] citations) {
+    //     Arrays.sort(citations);
+    //     int hIdx = 0;
+    //     for(int i = 0; i < citations.length; i++){
+    //         hIdx = Math.max(hIdx, Math.min(citations.length - i, citations[i]));
+    //     }
+    //     return hIdx;
+    // }
+    // count sort
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int hIdx = 0;
+        int[] count = new int[citations.length + 1];
         for(int i = 0; i < citations.length; i++){
-            hIdx = Math.max(hIdx, Math.min(citations.length - i, citations[i]));
+            count[Math.min(citations[i], citations.length)]++;
+        }
+        int hIdx = 0;
+        int numPaper = 0;
+        for(int i = count.length - 1; i >= 0; i--){
+            numPaper += count[i];
+            hIdx = Math.max(Math.min(i, numPaper), hIdx);
         }
         return hIdx;
     }
