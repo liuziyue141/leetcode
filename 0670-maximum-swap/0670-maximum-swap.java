55549262ea5b8b74b7d119e028b0ee7f7eb95875
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-06-11 13:19:33 -0700

    Time: 0 ms (100%), Space: 40.4 MB (65.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,33 +1,54 @@
 class Solution {
+
+    // // suboptimal approach
+    // public int maximumSwap(int num) {
+    //     String numStr = String.valueOf(num);
+    //     char[] numStrArr = numStr.toCharArray();
+    //     int max = 0;
+    //     int[] lastseen = new int[10];
+    //     for(int i = 0; i < numStr.length(); i++){
+    //         lastseen[(int)(numStrArr[i] - '0')] = i;
+    //     }
+    //     for(int i = 0; i < numStr.length(); i++){
+    //         int val = numStrArr[i] - '0';
+    //         if (val != max && i < lastseen[max]){
+    //             char temp = numStrArr[i];
+    //             numStrArr[i] = (char)(max + '0');
+    //             numStrArr[lastseen[max]] = temp;
+    //             return Integer.valueOf(new String(numStrArr));
+    //         }else if (val == max && i == lastseen[max]){
+    //             for(int j = val - 1; j > 0; j--){
+    //                 if(lastseen[j] != -1 && lastseen[j] > i){
+    //                     max = j;
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return num;
+    // }
+
+
+    // two-pass approach
     public int maximumSwap(int num) {
-        String numStr = String.valueOf(num);
-        char[] numStrArr = numStr.toCharArray();
-        int max = 0;
-        int[] count = new int[10];
-        Arrays.fill(count, -1);
-        for(int i = 0; i < numStr.length(); i++){
-            int val = numStr.charAt(numStr.length() - i - 1) - '0';
-            if (count[val] == -1){
-                count[val] = numStr.length() - i - 1;
-            }
-            if (val > max){
-                max = val;
+        char[] numStrArr = String.valueOf(num).toCharArray();
+        int n = numStrArr.length;
+        int[] rightMaxIndex = new int[n];
+        rightMaxIndex[n - 1] = n - 1;
+        for(int i = 1; i < n; i++){
+            char cur = numStrArr[n - i - 1];
+            if(numStrArr[rightMaxIndex[n - i]] >= cur){
+                rightMaxIndex[n - i - 1] = rightMaxIndex[n - i];
+            }else{
+                rightMaxIndex[n - i - 1] = n - i - 1;
             }
         }
-        for(int i = 0; i < numStr.length(); i++){
-            int val = numStrArr[i] - '0';
-            if (val != max && i < count[max]){
+        for(int i = 0; i < n; i++){
+            if(numStrArr[i] < numStrArr[rightMaxIndex[i]]){
                 char temp = numStrArr[i];
-                numStrArr[i] = (char)(max + '0');
-                numStrArr[count[max]] = temp;
+                numStrArr[i] = numStrArr[rightMaxIndex[i]];
+                numStrArr[rightMaxIndex[i]] = temp;
                 return Integer.valueOf(new String(numStrArr));
-            }else if (val == max && i == count[max]){
-                for(int j = val - 1; j > 0; j--){
-                    if(count[j] != -1 && count[j] > i){
-                        max = j;
-                        break;
-                    }
-                }
             }
         }
         return num;
