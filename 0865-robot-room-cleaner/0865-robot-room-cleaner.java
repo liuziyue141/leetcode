14071432034a41f043c2ac8d54d3cab032da8898
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-07-16 10:37:05 -0700

    Time: 7 ms (48.8%), Space: 45.5 MB (6.84%) - LeetHub

--- 0865-robot-room-cleaner/0865-robot-room-cleaner.java
+++ 0865-robot-room-cleaner/0865-robot-room-cleaner.java
@@ -18,44 +18,31 @@
 
 class Solution {
     Set<List<Integer>> visited; 
+    int[][] direction = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public void cleanRoom(Robot robot) {
         visited = new HashSet<>();
-        visited.add(Arrays.asList(0,0));
-        explore(robot, 0, 0, new int[]{1, 0});
+        explore(robot, 0, 0, 0);
     }
 
-    public void explore(Robot robot, int row, int col, int[] dir){
+    public void explore(Robot robot, int row, int col, int dir){
+        visited.add(Arrays.asList(row,col));
         robot.clean();
         for(int i = 0; i < 4; i++){
-            if(!visited.contains(Arrays.asList(row + dir[0], col + dir[1]))){
-                int newRow = row + dir[0];
-                int newCol = col + dir[1];
-                visited.add(Arrays.asList(newRow, newCol));
-                boolean canMove = robot.move();
-                if (canMove){
-                    explore(robot, newRow, newCol, dir);
-                    turnArround(robot, dir);
-                    robot.move();
-                    turnArround(robot, dir);
-                }
+            int newDir = (dir+i)%4;
+            int newRow = row + direction[newDir][0];
+            int newCol = col + direction[newDir][1];
+            if(!visited.contains(Arrays.asList(newRow, newCol))&& robot.move()){
+                explore(robot, newRow, newCol, newDir);
+                moveBack(robot);
             }
-            turnRight(robot, dir);
+            robot.turnRight();
         }
     }
 
-    public void turnRight(Robot robot, int[] dir){
-        if(dir[0] == 0){
-            dir[0] = dir[1];
-            dir[1] = 0;
-        }else{
-            dir[1] = -1 * dir[0];
-            dir[0] = 0;
-        }
+    public void moveBack(Robot robot){
         robot.turnRight();
-    }
-    public void turnArround(Robot robot, int[] dir){
-        dir[0] = -1*dir[0];
-        dir[1] = -1*dir[1];
+        robot.turnRight();
+        robot.move();
         robot.turnRight();
         robot.turnRight();
     }
