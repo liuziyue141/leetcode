9851929eaeabf898abbaae6616164dd216554da4
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-07-20 16:43:09 -0700

    Time: 0 ms (100%), Space: 41.1 MB (35.84%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.java
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.java
@@ -1,24 +1,18 @@
 class Solution {
-    public long distributeCandies(int n, int limit) {
-        // enumerate the candy one child has retrieved
-        // find the candy remaining
-        // if remaining / 2 > limit --> 0
-        // if remaining / 2 < limit & remaining <= limit --> remaining * 2 - (remaining + 1) %2
-        // if remaining / 2 < limit & remaining > limit --> (limit - (remaining - limit)) * 2 - (remaining + 1) %2
-        long sum = 0;
-        
-        for(int i = 0; i <= Math.min(n, limit); i++){
-            int remaining = n - i;
-            if((remaining + 1)/2 > limit){
-                continue;
-            }
-            if(remaining <= limit){
-                sum += remaining + 1;
-            }else{
-                sum += 2*limit - remaining + 1;
-            }
-        }
 
-        return sum;
+    public long distributeCandies(int n, int limit) {
+        return (
+            cal(n + 2) -
+            3 * cal(n - limit + 1) +
+            3 * cal(n - (limit + 1) * 2 + 2) -
+            cal(n - 3 * (limit + 1) + 2)
+        );
+    }
+
+    public long cal(int x) {
+        if (x < 0) {
+            return 0;
+        }
+        return ((long) x * (x - 1)) / 2;
     }
 }
