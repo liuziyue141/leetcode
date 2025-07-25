690b58799fb4189102d13e941932f13d389279bd
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-07-25 08:58:31 -0700

    Time: 41 ms (5.06%), Space: 60.8 MB (27.17%) - LeetHub

---
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int minCapability(int[] nums, int k) {
+        int max = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
+        }
+
+        while(min < max){
+            int mid = min + (max - min)/2;
+            boolean isPossible = check(nums, k, mid);
+            if(isPossible){
+                max = mid;
+            }else{
+                min = mid + 1; 
+            }
+        }
+        return min;
+    }
+
+    public boolean check(int[] nums, int k, int mid){
+        int[] dpSteal = new int[nums.length + 1];
+        int[] dpNotSteal = new int[nums.length + 1];
+        dpSteal[0] = k;
+        dpNotSteal[0] = k;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= mid){
+                dpSteal[i + 1] = dpNotSteal[i] - 1;
+            }else{
+                dpSteal[i + 1] = Math.min(dpNotSteal[i], dpSteal[i]);
+            }
+
+            dpNotSteal[i + 1] = Math.min(dpNotSteal[i], dpSteal[i]);
+
+            if(dpSteal[i+1] == 0 || dpNotSteal[i+1] == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+}
