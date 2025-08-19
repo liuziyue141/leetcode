e633ad799bbec44665e75ec496729af5ce25ba19
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-18 17:41:27 -0700

    Time: 1 ms (84.34%), Space: 40.7 MB (93.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -7,12 +7,10 @@ class Solution {
             int mid = left + (right - left) / 2;
             if(mid * mid > x){
                 right = mid - 1;
-            }else if(mid * mid == x){
-                return mid;
             }else{
                 left = mid + 1;
             }
         }
-        return left - 1;
+        return right;
     }
 }
