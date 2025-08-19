94b3ce89f459b786ec197270f22881379ac6e8ac
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-18 17:40:05 -0700

    Time: 1 ms (84.34%), Space: 41 MB (41.05%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mySqrt(int x) {
+        int right = 46340;
+        int left = 0;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(mid * mid > x){
+                right = mid - 1;
+            }else if(mid * mid == x){
+                return mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left - 1;
+    }
+}
