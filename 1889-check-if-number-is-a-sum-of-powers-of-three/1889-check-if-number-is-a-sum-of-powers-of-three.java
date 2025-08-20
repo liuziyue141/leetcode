0614063fa42fce8ea20d26b224d7cb26e516e9f5
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-20 10:47:17 -0700

    Time: 0 ms (100%), Space: 40.3 MB (91.17%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,18 +1,25 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int last = n;
-        int cur = (int)(Math.log(n) / Math.log(3));
-        while(cur < last){
-            // System.out.println(cur);
-            // System.out.println(n);
-            last = cur;
-            n = n - (int)Math.pow(3, cur);
-            if(n == 0){
-                return true;
-            }
-            cur = (int)(Math.log(n) / Math.log(3)+0.0001);
-
+        int factor = 1;
+        while(factor <= n){
+            factor *= 3;
         }
+        factor /= 3;
+
+        while(factor >= 1){
+            if(n >= factor){
+                n = n - factor;
+                if(n == 0){
+                    return true;
+                }
+                if(n >= factor){
+                    return false;
+                }
+            }
+            factor /= 3;
+        }
+
         return false;
+        
     }
 }
