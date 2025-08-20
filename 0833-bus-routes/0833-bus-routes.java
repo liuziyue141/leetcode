74e1a0ad392230d564f4f237ad3ec24b0e35c870
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-20 12:33:36 -0700

    Time: 51 ms (75.96%), Space: 56.1 MB (98.19%) - LeetHub

--- 0833-bus-routes/0833-bus-routes.java
+++ 0833-bus-routes/0833-bus-routes.java
@@ -1,58 +1,95 @@
 class Solution {
 
+    List<List<Integer>> adjList = new ArrayList();
+
+    // Iterate over each pair of routes and add an edge between them if there's a common stop.
+    void createGraph(int[][] routes) {
+        for (int i = 0; i < routes.length; i++) {
+            for (int j = i + 1; j < routes.length; j++) {
+                if (haveCommonNode(routes[i], routes[j])) {
+                    adjList.get(i).add(j);
+                    adjList.get(j).add(i);
+                }
+            }
+        }
+    }
+
+    // Returns true if the provided routes have a common stop, false otherwise.
+    boolean haveCommonNode(int[] route1, int[] route2) {
+        int i = 0, j = 0;
+        while (i < route1.length && j < route2.length) {
+            if (route1[i] == route2[j]) {
+                return true;
+            }
+
+            if (route1[i] < route2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+
+    // Add all the routes in the queue that have the source as one of the stops.
+    void addStartingNodes(Queue<Integer> q, int[][] routes, int source) {
+        for (int i = 0; i < routes.length; i++) {
+            if (isStopExist(routes[i], source)) {
+                q.add(i);
+            }
+        }
+    }
+
+    // Returns true if the given stop is present in the route, false otherwise.
+    boolean isStopExist(int[] route, int stop) {
+        for (int j = 0; j < route.length; j++) {
+            if (route[j] == stop) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int numBusesToDestination(int[][] routes, int source, int target) {
         if (source == target) {
             return 0;
         }
 
-        HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<>();
-        // Create a map from the bus stop to all the routes that include this stop.
-        for (int r = 0; r < routes.length; r++) {
-            for (int stop : routes[r]) {
-                // Add all the routes that have this stop.
-                ArrayList<Integer> route = adjList.getOrDefault(
-                        stop,
-                        new ArrayList<>());
-                route.add(r);
-                adjList.put(stop, route);
-            }
+        for (int i = 0; i < routes.length; ++i) {
+            Arrays.sort(routes[i]);
+            adjList.add(new ArrayList());
         }
 
-        if (adjList.get(source) == null || adjList.get(target) == null)
-            return -1;
+        createGraph(routes);
+
         Queue<Integer> q = new LinkedList<>();
-        Set<Integer> vis = new HashSet<Integer>(routes.length);
-        // Insert all the routes in the queue that have the source stop.
-        for (int route : adjList.get(source)) {
-            q.add(route);
-            vis.add(route);
-        }
+        addStartingNodes(q, routes, source);
 
+        Set<Integer> vis = new HashSet<Integer>(routes.length);
         int busCount = 1;
         while (!q.isEmpty()) {
-            int size = q.size();
+            int sz = q.size();
 
-            for (int i = 0; i < size; i++) {
-                int route = q.remove();
+            while (sz-- > 0) {
+                int node = q.remove();
 
-                // Iterate over the stops in the current route.
-                for (int stop : routes[route]) {
-                    // Return the current count if the target is found.
-                    if (stop == target) {
-                        return busCount;
-                    }
+                // Return busCount, if the stop target is present in the current route.
+                if (isStopExist(routes[node], target)) {
+                    return busCount;
+                }
 
-                    // Iterate over the next possible routes from the current stop.
-                    for (int nextRoute : adjList.get(stop)) {
-                        if (!vis.contains(nextRoute)) {
-                            vis.add(nextRoute);
-                            q.add(nextRoute);
-                        }
+                // Add the adjacent routes.
+                for (int adj : adjList.get(node)) {
+                    if (!vis.contains(adj)) {
+                        vis.add(adj);
+                        q.add(adj);
                     }
                 }
             }
+
             busCount++;
         }
+
         return -1;
     }
 }
