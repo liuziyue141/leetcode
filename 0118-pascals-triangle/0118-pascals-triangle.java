bbfbd1871cfd0ed8e7fbc19e5442e88b337c0453
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-27 17:37:55 -0700

    Time: 1 ms (86.27%), Space: 42.2 MB (40.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,15 +3,15 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         res.add(List.of(1));
         for(int i = 1; i < numRows; i++){
-            int[] curlevel = new int[i+1];
+            List<Integer> curlevel = new ArrayList<>();
             List<Integer> prev = res.get(i-1);
-            curlevel[0] = 1;
-            curlevel[i] = 1;
+            curlevel.add(1);
             for(int j = 1; j < i; j++){
                 int val = prev.get(j-1) + prev.get(j);
-                curlevel[j] = val;
+                curlevel.add(val);
             }
-            res.add(Arrays.stream(curlevel).boxed().collect(Collectors.toList()));
+            curlevel.add(1);
+            res.add(curlevel);
         }
         return res; 
     }
