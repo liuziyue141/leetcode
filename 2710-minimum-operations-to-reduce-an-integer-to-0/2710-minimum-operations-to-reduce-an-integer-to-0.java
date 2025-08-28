7db9443244d8d86268c5c8b11fccf3f5564f4a68
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-08-27 17:14:38 -0700

    Time: 0 ms (100%), Space: 40.4 MB (69.3%) - LeetHub

--- 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.java
+++ 2710-minimum-operations-to-reduce-an-integer-to-0/2710-minimum-operations-to-reduce-an-integer-to-0.java
@@ -1,24 +1,17 @@
+
+
+
 class Solution {
     public int minOperations(int n) {
         int totalOpts = 0;
         while(n > 0){
-            int cnt = 0;
-            while(n%2 == 0){
-                n = n>>1;
-            }
-            while(n%2 == 1){
-                cnt++;
+            if((n & 3) == 3){
+                totalOpts++;
+                n++;
+                //n = n >> 2;
+            }else{
+                totalOpts += (n & 1);
                 n = n >> 1;
-            }
-            if(cnt == 0){
-                n = n>>1;
-            }
-            if(cnt == 1){
-                totalOpts += 1;
-            }
-            if(cnt >= 2){
-                totalOpts += 1;
-                n = n + 1;
             }
         }
 
