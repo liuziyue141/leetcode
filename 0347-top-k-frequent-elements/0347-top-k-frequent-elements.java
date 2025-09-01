547539cbfa239a4b8e775ad3675cc1c5bbb2fe37
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-09-01 15:04:51 -0700

    Time: 12 ms (82.52%), Space: 48.1 MB (80.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -6,21 +6,40 @@ class Solution {
             e.frequency = e.frequency + 1;
             map.put(nums[i], e);
         }
-        
-        PriorityQueue<Element> pq = new PriorityQueue<Element>((Element a, Element b) -> {
-            return a.frequency - b.frequency;
-        });
-        for(Element entry: map.values()){
-            pq.add(entry);
-            if(pq.size() > k){
-                pq.poll();
-            }
-        }
+        List<Element> list = new ArrayList<>(map.values());
+        quickSelect(map.size() - k, list, 0, map.size() - 1);
         int[] res = new int[k];
-        for(int i = 0; i < k; i++){
-            res[i] = pq.poll().value;
+        int idx = 0;
+        for(int i = map.size() - k; i < map.size(); i++){
+            res[idx] = list.get(i).value;
+            idx++;
         }
         return res;
+    }
+
+    public void quickSelect(int k, List<Element> list, int start, int end){
+        int pivot = end;
+        int cur = start;
+        int pivotVal = list.get(pivot).frequency;
+        for(int i = start; i <= end; i++){
+            if(list.get(i).frequency < pivotVal){
+                Element temp = list.get(i);
+                list.set(i, list.get(cur));
+                list.set(cur, temp);
+                cur++;
+            }
+        }
+        Element temp = list.get(cur);
+        list.set(cur, list.get(pivot));
+        list.set(pivot, temp);
+
+        if(cur == k){
+            return;
+        }else if(cur > k){
+            quickSelect(k, list, start, cur - 1);
+        }else{
+            quickSelect(k, list, cur + 1, end);
+        }
     }
 
     class Element{
