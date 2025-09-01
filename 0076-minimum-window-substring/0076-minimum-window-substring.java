3f4d29a8ae9797d1742b80465ff562b88e9f2441
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-09-01 10:53:27 -0700

    Time: 16 ms (68.13%), Space: 45 MB (67.06%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,40 +1,43 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] curFreq = new int[128];
-        int[] targetFreq = new int[128];
-        int have = 0;
-        int need = 0;
+        Map<Character, Integer> freqMap = new HashMap<>();
         for(int i = 0; i < t.length(); i++){
-            if(targetFreq[t.charAt(i)]==0){
-                need++;
-            }
-            targetFreq[t.charAt(i)]++;
+            int cnt = freqMap.getOrDefault(t.charAt(i), 0) + 1;
+            freqMap.put(t.charAt(i), cnt);
         }
+        int size = freqMap.size();
 
-        //sliding window
         int left = 0;
         int right = 0;
-        int len = Integer.MAX_VALUE;
-        int[] res = new int[2];
+        String res = "";
+        int curSize = Integer.MAX_VALUE;
         while(right < s.length()){
-            curFreq[s.charAt(right)]++;
-            if(curFreq[s.charAt(right)] == targetFreq[s.charAt(right)]){
-                have++;
-            }
-            while(have == need){
-                curFreq[s.charAt(left)]--;
-                if(curFreq[s.charAt(left)] < targetFreq[s.charAt(left)]){
-                    have--;
-                    if(right - left + 1 < len){
-                        res[0] = left;
-                        res[1] = right;
-                        len = right - left + 1;
-                    }
+            if(freqMap.containsKey(s.charAt(right))){
+                int freq = freqMap.get(s.charAt(right))-1;
+                if(freq == 0){
+                    size--;
                 }
-                left++;
+                freqMap.put(s.charAt(right), freq);
+            }
+            if(size == 0){
+                while(size == 0){
+                    if(freqMap.containsKey(s.charAt(left))){
+                        int freq = freqMap.get(s.charAt(left));
+                        if(freq == 0){
+                            size++;
+                        }
+                        freqMap.put(s.charAt(left), freq + 1);
+                    }
+                    left++;
+                }
+                if(curSize > right - left + 2){
+                    res = s.substring(left - 1, right + 1);
+                    curSize = right - left + 2;
+                }
             }
             right++;
+
         }
-        return len == Integer.MAX_VALUE? "" : s.substring(res[0], res[1] + 1);
+        return res;
     }
 }
