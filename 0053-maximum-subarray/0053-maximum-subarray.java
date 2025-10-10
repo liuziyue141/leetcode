cc8331667614c38a08214146495b7fa0ae90a8e5
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-10-10 09:56:46 -0700

    Time: 1 ms (99.4%), Space: 57.1 MB (57.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,10 +3,8 @@ class Solution {
         int curMax = nums[0];
         int curVal = nums[0]; 
         for(int i = 1; i < nums.length; i++){
-            if(curVal < 0){
-                curVal = 0;
-            }
-            curVal += nums[i];
+            int num = nums[i];
+            curVal = Math.max(num, curVal + num);
             curMax = Math.max(curMax, curVal);
         }
         return curMax;
