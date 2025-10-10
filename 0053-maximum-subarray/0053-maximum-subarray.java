cad3b8c10e19b8d36793b24f6839ca025383fc13
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-10-09 21:54:46 -0700

    Time: 1 ms (99.4%), Space: 56.6 MB (95.13%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,40 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // return maxSubArray(nums, 0, nums.length - 1);
-
-        int lastMaxSub = nums[0];
-        int res = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            lastMaxSub = Math.max(lastMaxSub + nums[i], nums[i]);
-            res = Math.max(lastMaxSub, res);
+        int curMax = Integer.MIN_VALUE;
+        int curVal = 0; 
+        for(int i = 0; i < nums.length; i++){
+            curVal += nums[i];
+            curMax = Math.max(curMax, curVal);
+            if(curVal < 0){
+                curVal = 0;
+            }
         }
-        System.gc();
-        return res;
-        
+        return curMax;
     }
-
-    // public int maxSubArray(int[] nums, int start, int end){
-    //     if(start == end){
-    //         return nums[start];
-    //     }
-    //     int mid = (start+end)/2;
-    //     int leftmax = maxSubArray(nums, start, mid);
-    //     int rightmax = maxSubArray(nums, mid+1, end);
-    //     int middle = nums[mid] + nums[mid + 1];
-    //     int rightCur = 0;
-    //     int rightMax = 0;
-    //     for(int i = mid + 2; i <= end; i++){
-    //         rightCur += nums[i];
-    //         rightMax = Math.max(rightMax, rightCur);
-    //     }
-    //     int leftCur = 0;
-    //     int leftMax = 0;
-    //     for(int i = mid - 1; i >= start; i--){
-    //         leftCur += nums[i];
-    //         leftMax = Math.max(leftMax, leftCur);
-    //     }
-
-    //     middle = middle + rightMax + leftMax;
-    //     return Math.max(middle, Math.max(leftmax, rightmax));
-    // }
 }
