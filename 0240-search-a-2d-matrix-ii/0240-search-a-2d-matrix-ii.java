c2432f1a6fc9653a9e35044229d8b4332d212191
liuziyue141 <111166855+liuziyue141@users.noreply.github.com>
2025-10-27 15:46:37 -0700

    Time: 21 ms (8.4%), Space: 48.5 MB (6.28%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -4,26 +4,51 @@ class Solution {
         int j = 0;
 
         while(j < matrix[0].length && i >= 0){
-            while(j < matrix[0].length && matrix[i][j] < target){
-                j++;
+
+            // use binary search to find the minimum value that is greater than target
+            int l = j;
+            int r = matrix[0].length - 1;
+            int res = -1;
+            while(l <= r){
+                int mid = l + (r - l) / 2;
+                if(matrix[i][mid] == target){
+                    return true;
+                }else if(matrix[i][mid] > target){
+                    res = mid;
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+
             }
-            if(j == matrix[0].length){
+            System.out.println(res);
+            if(res == -1){
+                return false;
+            }
+             j = res;
+
+            
+
+            // use binary search to find the maximum value that is less than target
+            l = 0;
+            r = i;
+            res = -1;
+            while(l <= r){
+                int mid = l + (r - l) / 2;
+                if(matrix[mid][j] == target){
+                    return true;
+                }else if(matrix[mid][j] > target){
+                    r = mid - 1;
+                }else{
+                    res = mid;
+                    l = mid + 1;
+                }
+            }
+            if(res == -1){
                 return false;
             }
 
-            if(matrix[i][j] == target){
-                return true;
-            }
-
-            while(i>=0 && matrix[i][j] > target){
-                i--;
-            }
-            if(i < 0){
-                return false;
-            }
-            if(matrix[i][j] == target){
-                return true;
-            }
+            i = res;
         }
 
         return false;
